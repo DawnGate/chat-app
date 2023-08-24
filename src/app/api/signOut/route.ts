3214527acb03c8c9546bd561b5b840040a639447
DawnGate <lam99quyen@gmail.com>
2023-08-24 23:17:57 +0700

    [dev] fix problem with eslint for build app

--- src/app/api/signOut/route.ts
+++ src/app/api/signOut/route.ts
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
+// eslint-disable-next-line import/prefer-default-export
 export async function POST(request: NextRequest) {
   const options = {
     name: 'session',
@@ -10,5 +11,3 @@ export async function POST(request: NextRequest) {
   cookies().set(options);
   return NextResponse.json({}, { status: 200 });
 }
-
-export default POST;
